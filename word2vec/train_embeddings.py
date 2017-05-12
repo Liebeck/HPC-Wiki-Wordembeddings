fbed01d916354991ee92ae021f3bdac695d13236
Matthias Liebeck <Liebeck@users.noreply.github.com>
2017-05-12 14:11:46 +0200

    Use argparse

--- word2vec/train_embeddings.py
+++ word2vec/train_embeddings.py
@@ -5,31 +5,31 @@
 import logging
 import os.path
 import sys
-import multiprocessing
-
-from gensim.corpora import WikiCorpus
+import argparse
 from gensim.models import Word2Vec
 from gensim.models.word2vec import LineSentence
 
+
+def config_argparser():
+    argparser = argparse.ArgumentParser(description='ArgMining')
+    argparser.add_argument('-input_path', type=str, required=True, help='Path to the preprocessed Wikipedia dump')
+    argparser.add_argument('-output_path', type=str, required=True, help='Write path for the word2vec models')
+    argparser.add_argument('-dimension', type=int, help='Size of the embeddings')
+    return argparser.parse_args()
+
+
 if __name__ == '__main__':
+    arguments = config_argparser()
     program = os.path.basename(sys.argv[0])
     logger = logging.getLogger(program)
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s')
     logging.root.setLevel(level=logging.INFO)
     logger.info("running %s" % ' '.join(sys.argv))
-    # check and process input arguments
-    input_path_wiki = sys.argv[1]
-    output_embedding_path = sys.argv[2]
-    size = int(sys.argv[3])
-    logger.info("Input path wiki: " + input_path_wiki)
-    logger.info("Word embedding output path: " + output_embedding_path)
-    logger.info("Size: " + str(size))
-    if len(sys.argv) < 3:
-        sys.exit(1)
-
-    model = Word2Vec(LineSentence(input_path_wiki), size=size, window=5, min_count=5, workers=20)
+    logger.info("Input path wiki: " + arguments.input_path)
+    logger.info("Word embedding output path: " + arguments.output_path)
+    logger.info("Size: " + arguments.dimension)
+    model = Word2Vec(LineSentence(arguments.input_path), size=arguments.dimension, window=5, min_count=5, workers=20)
     # trim unneeded model memory = use (much) less RAM
     model.init_sims(replace=True)
-
-    model.save(output_embedding_path + "_binary")
-    model.save_word2vec_format(output_embedding_path, binary=False)
+    model.save(arguments.output_path + "_binary")
+    model.save_word2vec_format(arguments.output_path, binary=False)
